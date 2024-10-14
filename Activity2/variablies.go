3481445e352202d8a8a8ee318f4334ef6aea01ab
EstifanosTaye320 <estifanostaye11@gmail.com>
2024-10-14 07:00:33 +0300

    Activity 2

---
+++ Activity2/variablies.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+		var a int = 10
+	var b float64 = 20.5
+	var c string = "Golang"
+	var d bool = true
+
+	e:= "something"
+
+	fmt.Println(a)
+	fmt.Println(b)
+	fmt.Println(c)
+	fmt.Println(d)
+	fmt.Println(e)
+	fmt.Println("Hello World!")
+}
